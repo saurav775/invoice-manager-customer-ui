a71f67d6152bda93f98d3a1bb0499db0448676e2
saurav775 <sauravchoudhury775@gmail.com>
2024-07-16 13:31:38 +0530

    fix: add remotes webpack prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -17,8 +17,28 @@ module.exports = () => {
       new ModuleFederationPlugin({
         name: "invoice_manager_customer_ui",
         filename: "remoteEntry.js",
+        remotes: {
+          invoice_manager_dashboard_ui:
+            "invoice_manager_dashboard_ui@http://localhost:3000/remoteEntry.js",
+          invoice_manager_customer_ui:
+            "invoice_manager_customer_ui@http://localhost:3001/remoteEntry.js",
+        },
         exposes: {
           "./CustomersPage": "./src/Bootstrap",
+          "./TableUI": "./src/components/common/TableUI",
+          "./Button": "./src/components/common/Button",
+          "./RightDrawer": "./src/components/common/RightDrawer",
+          "./Loader": "./src/components/common/Loader",
+          "./Snackbar": "./src/components/common/Snackbar",
+          "./Popup": "./src/components/common/Popup",
+          "./CustomerForm": "./src/components/CustomerForm",
+          "./constants": "./src/constants",
+          "./actions/rightDrawerActions":
+            "./src/store/actions/rightDrawer.actions",
+          "./actions/snackBarActions": "./src/store/actions/snackBar.actions",
+          "./actions/popupActions": "./src/store/actions/popup.actions",
+          "./actions/customerActions": "./src/store/actions/customers.actions",
+          "./hooks/useForm": "./src/hooks/useForm",
         },
         shared: packageJson.dependencies,
       }),
